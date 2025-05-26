fcea55ae760e63162c4e0cea0d61cdf77dd2c87b
Tarobot2 <2003taro@gmail.com>
2025-05-26 09:20:46 +0900

    Add files via upload

---
+++ 太郎bot/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import os
+import random
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi(os.environ['LINE_CHANNEL_ACCESS_TOKEN'])
+handler = WebhookHandler(os.environ['LINE_CHANNEL_SECRET'])
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    replies = [
+        "やっほー！",
+        "今日も元気しとー？",
+        "何しよると〜？",
+        "ヒマなん？笑",
+        "おお、話しかけてくれてありがと！"
+    ]
+    reply = random.choice(replies)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply)
+    )
+
+if __name__ == "__main__":
+    app.run()
